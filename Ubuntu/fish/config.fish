b2a0e6c0d48f20ac57526f1d5e33df3465fc5a59
Brian Kenkel <briankenkel.t@gmail.com>
2026-02-11 21:46:14 -0500

    remove secrets.fish source

--- Ubuntu/fish/config.fish
+++ Ubuntu/fish/config.fish
@@ -5,6 +5,5 @@ end
 set -gx PATH "$HOME/.local/bin" $PATH
 set -gx PATH "/usr/local/go/bin" $PATH
 
-# source $HOME/secrets.fish
 starship init fish | source
 /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
