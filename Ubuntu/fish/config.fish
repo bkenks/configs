0820c54c65ee509f9441a023dcf3ab0cd9382e72
Brian Kenkel <briankenkel.t@gmail.com>
2026-02-11 21:46:29 -0500

    add ssh term and alias for kitty

--- Ubuntu/fish/config.fish
+++ Ubuntu/fish/config.fish
@@ -5,5 +5,11 @@ end
 set -gx PATH "$HOME/.local/bin" $PATH
 set -gx PATH "/usr/local/go/bin" $PATH
 
+if test $TERM = xterm-kitty
+    function ssh
+        kitty +kitten ssh $argv
+    end
+end
+
 starship init fish | source
 /home/linuxbrew/.linuxbrew/bin/brew shellenv | source
